e5a8f0c6100b5e01c65182982bca1c7d8905dab9
Kekechi <56617711+Kekechi@users.noreply.github.com>
2022-01-19 16:15:07 -0800

    add package utils

--- src/main/java/utils/LogWriter.java
+++ src/main/java/utils/LogWriter.java
@@ -1,3 +1,5 @@
+package utils;
+
 import java.io.File;  // Import the File class
 import java.io.FileWriter;   // Import the FileWriter class
 import java.io.IOException;  // Import the IOException class to handle errors
